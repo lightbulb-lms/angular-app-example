325e22d62550d068d95f4a913570107ee1bdb638
Christopher Nellis <christopher.t.nellis@gmail.com>
2020-10-09 10:07:55 -0400

    Move component for house keeping purposes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
