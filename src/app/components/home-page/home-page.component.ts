e9df6d289bc4a2bd63e009fee510edfb752eaf17
Christopher Nellis <christopher.t.nellis@gmail.com>
2020-10-09 10:36:22 -0400

    Import jokeService to home component

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {JokeService} from '../../service/joke/joke.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jokeService: JokeService) { }
 
   ngOnInit(): void {
   }
